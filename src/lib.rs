5a16b79cd7857cd23656bca42d72fb5697593635
kgeronim <kevin.geronimo@outlook.com>
2020-01-28 23:16:29 +0000

    signals finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use cbpro::{
-    client::{PublicClient, MAIN_URL},
-    websocket::{Channels, WebSocketFeed, MAIN_FEED_URL}
+    client::{PublicClient, MAIN_URL, SANDBOX_URL},
+    websocket::{Channels, WebSocketFeed, MAIN_FEED_URL, SANDBOX_FEED_URL}
 };
 use chrono::{DateTime, NaiveDateTime, Duration, Utc};
 use tokio::{sync::mpsc, task::JoinHandle};
@@ -9,9 +9,20 @@ use std::collections::VecDeque;
 
 pub type Candle = (DateTime<Utc>, f64, f64, f64, f64, f64, (&'static str, f64, f64));
 
-pub async fn candles(product_id: &str, granularity: i64) -> cbpro::error::Result<(JoinHandle<()>, impl Stream<Item=Candle> + Unpin + Send + 'static)> {
+#[derive(Copy, Clone)]
+pub enum Source {
+    Main,
+    Sandbox
+}
 
-    let client = PublicClient::new(MAIN_URL);
+pub async fn candles(product_id: &str, granularity: i64, source: Source) -> cbpro::error::Result<(JoinHandle<()>, impl Stream<Item=Candle> + Unpin + Send + 'static)> {
+    let (cb_url, feed_url) = if let Source::Main = source {
+        (MAIN_URL, MAIN_FEED_URL)
+    } else {
+        (SANDBOX_URL, SANDBOX_FEED_URL)
+    };
+
+    let client = PublicClient::new(cb_url);
     let end = Utc::now(); // - Duration::seconds(60);
     let start = end - Duration::seconds(300 * granularity);
     
@@ -35,7 +46,7 @@ pub async fn candles(product_id: &str, granularity: i64) -> cbpro::error::Result
 
     let rates = stream::iter(rates);
 
-    let mut feed = WebSocketFeed::connect(MAIN_FEED_URL).await?;
+    let mut feed = WebSocketFeed::connect(feed_url).await?;
     feed.subscribe(&[product_id], &[Channels::TICKER]).await?;
 
     let (mut tx, rx) = mpsc::channel(100);
@@ -95,7 +106,8 @@ pub async fn candles(product_id: &str, granularity: i64) -> cbpro::error::Result
     Ok((handle, rates))
 }
 
-pub async fn ema<T>(mut candles: T, window_size: usize) -> cbpro::error::Result<(JoinHandle<()>, impl Stream<Item=Option<(DateTime<Utc>, f64)>> + Unpin + Send + 'static)>
+type Ema = Option<(DateTime<Utc>, f64, (&'static str, f64, f64))>;
+pub async fn ema<T>(mut candles: T, window_size: usize) -> cbpro::error::Result<(JoinHandle<()>, impl Stream<Item=Ema> + Unpin + Send + 'static)>
 where 
     T: Stream<Item=Candle> + Unpin + Send + 'static,
 {
@@ -108,7 +120,7 @@ where
         let mut prev_ema = None::<f64>;
         let mut window: VecDeque<f64> = VecDeque::with_capacity(window_size);
 
-        while let Some((time, .., close, _, _)) = candles.next().await {
+        while let Some((time, .., close, _, direction)) = candles.next().await {
             if count == window_size - 1 {
                 window.push_back(close);
                 let sma = window.iter().sum::<f64>() / window_size as f64;
@@ -117,7 +129,7 @@ where
                     let ema = close * k + ema * (1.0 - k);
                     prev_ema = Some(ema);
 
-                    if let Err(_) = tx.send(Some((time, ema))).await {
+                    if let Err(_) = tx.send(Some((time, ema, direction))).await {
                         println!("receiver dropped");
                         return;
                     }
@@ -141,15 +153,16 @@ where
     Ok((handle, rx))
 }
 
-pub async fn rsi(product_id: &str, granularity: i64) -> cbpro::error::Result<(JoinHandle<()>, impl Stream<Item=Option<(DateTime<Utc>, f64)>> + Unpin + Send + 'static)> {
+type Rsi = Option<(DateTime<Utc>, f64, (&'static str, f64, f64))>;
+pub async fn rsi(product_id: &str, granularity: i64, source: Source) -> cbpro::error::Result<(JoinHandle<()>, impl Stream<Item=Rsi> + Unpin + Send + 'static)> {
     let (mut tx, rx) = mpsc::channel(100);
 
     let size: usize = 14;
-    let (_, gains) = candles(product_id, granularity).await?;
+    let (_, gains) = candles(product_id, granularity, source).await?;
     let gains = gains.map(|mut x|if (x.4 - x.1).is_sign_positive() {x} else {x.4 = 0.; x});
     let (_, mut gains) = ema(gains, size).await?;
 
-    let (_, losses) = candles(product_id, granularity).await?;
+    let (_, losses) = candles(product_id, granularity, source).await?;
     let losses = losses.map(|mut x|if (x.4 - x.1).is_sign_negative() {x} else {x.4 = 0.; x});
     let (_, mut losses) = ema(losses, size).await?;
 
@@ -157,9 +170,9 @@ pub async fn rsi(product_id: &str, granularity: i64) -> cbpro::error::Result<(Jo
     let handle = tokio::spawn(async move {
         while let (Some(gains), Some(losses)) = join!(gains.next(), losses.next()) {
 
-            if let (Some((_, gains)), Some((time, losses))) = (gains, losses) {
+            if let (Some((_, gains, direction)), Some((time, losses, _))) = (gains, losses) {
                 let rsi = 100.0 - (100.0 / (1.0 + (gains / losses)));
-                if let Err(_) = tx.send(Some((time, rsi))).await {
+                if let Err(_) = tx.send(Some((time, rsi, direction))).await {
                     println!("receiver dropped");
                     return;
                 }
@@ -176,22 +189,23 @@ pub async fn rsi(product_id: &str, granularity: i64) -> cbpro::error::Result<(Jo
     Ok((handle, rx))
 }
 
-pub async fn macd(product_id: &str, granularity: i64) -> cbpro::error::Result<(JoinHandle<()>, impl Stream<Item=Option<(DateTime<Utc>, f64)>> + Unpin + Send + 'static)> {
+type Macd = Option<(DateTime<Utc>, f64, (&'static str, f64, f64))>;
+pub async fn macd(product_id: &str, granularity: i64, source: Source) -> cbpro::error::Result<(JoinHandle<()>, impl Stream<Item=Macd> + Unpin + Send + 'static)> {
     let (mut tx, rx) = mpsc::channel(100);
 
-    let (_, ema12) = candles(product_id, granularity).await?;
+    let (_, ema12) = candles(product_id, granularity, source).await?;
     let (_, mut ema12) = ema(ema12, 12).await?;
 
-    let (_, ema26) = candles(product_id, granularity).await?;
+    let (_, ema26) = candles(product_id, granularity, source).await?;
     let (_, mut ema26) = ema(ema26, 26).await?;
 
 
     let handle = tokio::spawn(async move {
         while let (Some(ema12), Some(ema26)) = join!(ema12.next(), ema26.next()) {
             
-            if let (Some((_, ema12)), Some((time, ema26))) = (ema12, ema26) {
+            if let (Some((_, ema12, _)), Some((time, ema26, direction))) = (ema12, ema26) {
                 let macd = ema12 - ema26;
-                if let Err(_) = tx.send(Some((time, macd))).await {
+                if let Err(_) = tx.send(Some((time, macd, direction))).await {
                     println!("receiver dropped");
                     return;
                 }
--- src/main.rs
+++ src/main.rs
@@ -1,188 +1,68 @@
-use futures::stream::StreamExt;
-use tokio_postgres::{types::ToSql, NoTls};
-use futures::try_join;
-use chrono::{DateTime, Utc};
+use futures::{stream::StreamExt, join};
+use cbbot::Source::Main;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let connection_string = format!("host=timescaledb user=postgres password=test123");
-    let (db_client, connection) =
-        tokio_postgres::connect(&connection_string, NoTls).await?;
+    let (_, mut rsi) = cbbot::rsi("BTC-USD", 60, Main).await?;
+    let (_, mut macd) = cbbot::macd("BTC-USD", 60, Main).await?;
 
-    let db_client = std::sync::Arc::new(db_client);
-    let db_client1 = std::sync::Arc::clone(&db_client);
-    let db_client2 = std::sync::Arc::clone(&db_client);
-    let db_client3 = std::sync::Arc::clone(&db_client);
-    let db_client4 = std::sync::Arc::clone(&db_client);
-    let db_client5 = std::sync::Arc::clone(&db_client);
+    let mut buy_state = State {prev_macd: None, confirm_count: 0};
+    let mut sell_state = State {prev_macd: None, confirm_count: 0};
 
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            println!("connection error: {}", e);
-        }
-    });
+    while let (Some(rsi), Some(macd)) = join!(rsi.next(), macd.next()) {
+        if let (Some((time, rsi, (_, _, _))), Some((_, macd, _))) = (rsi, macd) {
+            
+            // Buy signals happen here
+            if rsi < 30.0 {
+                if let None = buy_state.prev_macd {
+                    buy_state.prev_macd = Some(macd);
+                    println!("rsi buy signal, time: {}, rsi: {}", time, rsi)
+                }
+            }
 
-    db_client.simple_query(
-        "CREATE TABLE ma_close (
-            id          SERIAL PRIMARY KEY,
-            time        TIMESTAMP WITH TIME ZONE NOT NULL,
-            close       FLOAT NOT NULL
-        )"
-    ).await?;
+            if let Some(pm) = buy_state.prev_macd {
+                if macd > pm {
+                    buy_state.confirm_count += 1;
+                    println!("macd buy signal, time: {}, macd: {}, prev_macd: {:?}, confirm: {}", time, macd, buy_state.prev_macd, buy_state.confirm_count);
+                }
+                buy_state.prev_macd = Some(macd);
+            }
 
-    db_client.simple_query(
-        "CREATE TABLE ma_emat (
-            id          SERIAL PRIMARY KEY,
-            time        TIMESTAMP WITH TIME ZONE,
-            emat        FLOAT
-        )"
-    ).await?;
+            if buy_state.confirm_count > 1 {
+                println!("Buy at, time:{}, rsi: {}, macd: {}, confirm: {}", time, rsi, macd, buy_state.confirm_count);
+                buy_state.confirm_count = 0;
+                buy_state.prev_macd = None;
+            }
 
-    db_client.simple_query(
-        "CREATE TABLE ma_ematw (
-            id          SERIAL PRIMARY KEY,
-            time        TIMESTAMP WITH TIME ZONE,
-            ematw       FLOAT
-        )"
-    ).await?;
+            // Sell signals happen here
+            if rsi > 70.0 {
+                if let None = sell_state.prev_macd {
+                    sell_state.prev_macd = Some(macd);
+                    println!("rsi sell signal, time: {}, rsi: {}", time, rsi)
+                }
+            }
 
-    db_client.simple_query(
-        "CREATE TABLE ma_rsi (
-            id          SERIAL PRIMARY KEY,
-            time        TIMESTAMP WITH TIME ZONE,
-            rsi         FLOAT
-        )"
-    ).await?;
+            if let Some(pm) = sell_state.prev_macd {
+                if macd < pm {
+                    sell_state.confirm_count += 1;
+                    println!("macd sell signal, time: {}, macd: {}, prev_macd: {:?}, confirm: {}", time, macd, sell_state.prev_macd, sell_state.confirm_count);
+                }
+                sell_state.prev_macd = Some(macd);
+            }
 
-    db_client.simple_query(
-        "CREATE TABLE ma_macd (
-            id          SERIAL PRIMARY KEY,
-            time        TIMESTAMP WITH TIME ZONE,
-            macd        FLOAT
-        )"
-    ).await?;
-
-    let statement_close = db_client.prepare(
-        "INSERT INTO ma_close (time, close) VALUES ($1, $2)"
-    ).await.unwrap();
-
-    let statement_ema12 = db_client.prepare(
-        "INSERT INTO ma_emat (time, emat) VALUES ($1, $2)"
-    ).await.unwrap();
-
-    let statement_ema26 = db_client.prepare(
-        "INSERT INTO ma_ematw (time, ematw) VALUES ($1, $2)"
-    ).await.unwrap();
-
-    let statement = db_client.prepare(
-        "INSERT INTO ma_macd (time, macd) VALUES ($1, $2)"
-    ).await.unwrap();
-
-    let statement_rsi = db_client.prepare(
-        "INSERT INTO ma_rsi (time, rsi) VALUES ($1, $2)"
-    ).await.unwrap();
-
-    let (r1, close) = cbbot::candles("BTC-USD", 60).await?;
-    let (r2, mut ema12) = cbbot::ema(close, 12).await?;
-
-    let (r3, close) = cbbot::candles("BTC-USD", 60).await?;
-    let (r4, mut ema26) = cbbot::ema(close, 26).await?;
-
-
-    let (r5, mut rsi) = cbbot::rsi("BTC-USD", 60).await?;
-    let (r6, mut macd) = cbbot::macd("BTC-USD", 60).await?;
-    
-    let (r7, mut close) = cbbot::candles("BTC-USD", 60).await?;
-    tokio::spawn(async move {
-        while let Some((time, .., close, _, _)) = close.next().await {
-
-            let params: &[&(dyn ToSql + Sync)] = &[&time, &close];
-            if let Err(e) = db_client1.execute(&statement_close, params).await {
-                print!("{:?}", e);
-                return;
-            }    
-        }
-    });
-
-    tokio::spawn(async move {
-        while let Some(ema12) = ema12.next().await {
-
-            if let Some((time, ema12)) = ema12 {
-                let params: &[&(dyn ToSql + Sync)] = &[&time, &ema12];
-                if let Err(e) = db_client2.execute(&statement_ema12, params).await {
-                    print!("{:?}", e);
-                    return;
-                }   
-            } else {
-                let params: &[&(dyn ToSql + Sync)] = &[&None::<DateTime<Utc>>, &None::<f64>];
-                if let Err(e) = db_client2.execute(&statement_ema12, params).await {
-                    print!("{:?}", e);
-                    return;
-                }   
+            if sell_state.confirm_count > 1 {
+                println!("Sell at, time:{}, rsi: {}, macd: {}, confirm: {}", time, rsi, macd, sell_state.confirm_count);
+                sell_state.confirm_count = 0;
+                sell_state.prev_macd = None;
             }
         }
-    });
+    }
 
-    tokio::spawn(async move {
-        while let Some(ema26) = ema26.next().await {
-
-            if let Some((time, ema26)) = ema26 {
-                let params: &[&(dyn ToSql + Sync)] = &[&time, &ema26];
-                if let Err(e) = db_client3.execute(&statement_ema26, params).await {
-                    print!("{:?}", e);
-                    return;
-                } 
-            } else {
-                let params: &[&(dyn ToSql + Sync)] = &[&None::<DateTime<Utc>>, &None::<f64>];
-                if let Err(e) = db_client3.execute(&statement_ema26, params).await {
-                    print!("{:?}", e);
-                    return;
-                } 
-            }
-        }
-    });
-
-    tokio::spawn(async move {
-        while let Some(macd) = macd.next().await {
-
-            if let Some((time, macd)) = macd {
-                let params: &[&(dyn ToSql + Sync)] = &[&time, &macd];
-                if let Err(e) = db_client4.execute(&statement, params).await {
-                    print!("{:?}", e);
-                    return;
-                }    
-            } else {
-                let params: &[&(dyn ToSql + Sync)] = &[&None::<DateTime<Utc>>, &None::<f64>];
-                if let Err(e) = db_client4.execute(&statement, params).await {
-                    print!("{:?}", e);
-                    return;
-                }    
-            }
-        }
-    });
-
-    tokio::spawn(async move {
-        while let Some(rsi) = rsi.next().await {
-
-            if let Some((time, rsi)) = rsi {
-                let params: &[&(dyn ToSql + Sync)] = &[&time, &rsi];
-                if let Err(e) = db_client5.execute(&statement_rsi, params).await {
-                    print!("{:?}", e);
-                    return;
-                }    
-            } else {
-                let params: &[&(dyn ToSql + Sync)] = &[&None::<DateTime<Utc>>, &None::<f64>];
-                if let Err(e) = db_client5.execute(&statement_rsi, params).await {
-                    print!("{:?}", e);
-                    return;
-                }    
-            }
-        }
-    });
-
-    try_join!(r1, r2, r3, r4, r5, r6, r7)?;
-
-    //println!("{}", serde_json::to_string_pretty(&value).unwrap());
     Ok(())
+}
+
+struct State {
+    prev_macd: Option<f64>,
+    confirm_count: usize
 }
